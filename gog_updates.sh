d8844751dd971d3eac5ce047f901eff3d3275e78
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2023-06-12 20:27:45 +0300

    Include the new product release date timeline plot_gen option in scan script

--- gog_updates.sh
+++ gog_updates.sh
@@ -22,7 +22,8 @@ python3 gog_prices_scan.py -u
 python3 gog_ratings_scan.py -u
 
 #uncomment if you want to generate statistical charts
-#python3 gog_plot_gen.py -t
+#python3 gog_plot_gen.py -t -f id
+#python3 gog_plot_gen.py -t -f release
 #python3 gog_plot_gen.py -d
 #only relevant if you set the correct CUTOFF_DATE
 #python3 gog_plot_gen.py -i
